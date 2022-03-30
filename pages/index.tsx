6aaccb64cc16d7ff597dda5a6de2d8411d9bf041
Tripouille <jmichel.gambard@gmail.com>
2022-03-30 19:13:30 +0200

    feat(home-page): add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import Header from '@/components/Header';
 import UserInformations from '@/components/UserInformations';
 import type { NextPage } from 'next';
+import { chakra, Box } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <UserInformations />
-    </div>
+    <Box bgColor="gray.100" h="100vh">
+      <Header />
+      <chakra.main px={[5, 20]} py={5}>
+        <UserInformations />
+      </chakra.main>
+    </Box>
   );
 };
 
