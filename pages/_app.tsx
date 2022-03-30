663ba9051c4ea2aebe6f5e468378815898ba8c5d
Tripouille <jmichel.gambard@gmail.com>
2022-03-30 16:58:21 +0200

    feat(redux): wrap app in provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from 'store';
 import theme from 'theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </ChakraProvider>
   );
 }
