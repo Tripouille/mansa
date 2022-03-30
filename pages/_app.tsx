baef2b7ea16b8ef6cfbf3b539cb6b3e0151ef8ee
Tripouille <jmichel.gambard@gmail.com>
2022-03-30 15:36:06 +0200

    feat(chakra): add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import theme from 'theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      primary: '#6347D1',
+    },
+  },
+});
+
+export default theme;
