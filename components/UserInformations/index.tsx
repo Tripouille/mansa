98dac67b340349eeba72b4c5599dd96e0c2d585c
Tripouille <jmichel.gambard@gmail.com>
2022-03-30 16:59:00 +0200

    feat(user-informations): improve UserInformations component

--- components/UserInformations/index.tsx
+++ components/UserInformations/index.tsx
@@ -1,15 +1,12 @@
 import { FC } from 'react';
-import { Avatar, HStack, Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+import { Avatar, HStack, Skeleton, SkeletonCircle } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
+import { useUserQuery } from 'services/user';
 
-export interface UserInformationsProps {
-  firstname: string;
-  lastname: string;
-  isLoading: boolean;
-}
+const UserInformations: FC = () => {
+  const { data: user, isFetching, isError, isSuccess } = useUserQuery();
 
-const UserInformations: FC<UserInformationsProps> = ({ firstname, lastname, isLoading }) => {
-  if (isLoading) {
+  if (isFetching) {
     return (
       <HStack align="center" spacing={3} padding={3}>
         <SkeletonCircle size="50px" flexShrink={0} />
@@ -18,14 +15,29 @@ const UserInformations: FC<UserInformationsProps> = ({ firstname, lastname, isLo
     );
   }
 
-  return (
-    <HStack align="center" spacing={3} padding={3}>
-      <Avatar name={`${lastname} ${firstname}`} bgColor="primary" />
-      <Heading as="h1" fontWeight="semibold">
-        {lastname.toUpperCase()} {firstname}
-      </Heading>
-    </HStack>
-  );
+  if (isError) {
+    return (
+      <HStack align="center" spacing={3} padding={3}>
+        <Heading as="h1" fontWeight="semibold">
+          Unaivalable user informations
+        </Heading>
+      </HStack>
+    );
+  }
+
+  if (isSuccess) {
+    const { first: firstname, last: lastname } = user.name;
+
+    return (
+      <HStack align="center" spacing={3} padding={3}>
+        <Avatar name={`${lastname} ${firstname}`} bgColor="primary" />
+        <Heading as="h1" fontWeight="semibold">
+          {lastname.toUpperCase()} {firstname}
+        </Heading>
+      </HStack>
+    );
+  }
+  return null;
 };
 
 export default UserInformations;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <UserInformations firstname="Rémy" lastname="Tinco" isLoading />
+      <UserInformations />
     </div>
   );
 };
