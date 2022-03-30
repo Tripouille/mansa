cfcd84e6f1c9604fbfdd3cd96cec79e3cdfed516
Tripouille <jmichel.gambard@gmail.com>
2022-03-30 20:02:14 +0200

    feat(home-page): add CompanyInformations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import Header from '@/components/Header';
 import UserInformations from '@/components/UserInformations';
 import type { NextPage } from 'next';
-import { chakra, Box } from '@chakra-ui/react';
+import { chakra, Box, VStack } from '@chakra-ui/react';
+import CompanyInformations from '@/components/CompanyInformations';
 
 const Home: NextPage = () => {
   return (
     <Box bgColor="gray.100" h="100vh">
       <Header />
       <chakra.main px={[5, 20]} py={5}>
-        <UserInformations />
+        <VStack align='left' spacing={10}>
+            <UserInformations />
+            <CompanyInformations />
+        </VStack>
       </chakra.main>
     </Box>
   );
