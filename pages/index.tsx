ad1440ffb5fe086b8147206db2ef61035183d6f9
Tripouille <jmichel.gambard@gmail.com>
2022-03-31 14:14:41 +0200

    feat(home-page): add Accounts, improve style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,17 @@ import UserInformations from '@/components/UserInformations';
 import type { NextPage } from 'next';
 import { chakra, Box, VStack } from '@chakra-ui/react';
 import CompanyInformations from '@/components/CompanyInformations';
+import Accounts from '@/components/Accounts';
 
 const Home: NextPage = () => {
   return (
-    <Box bgColor="gray.100" h="100vh">
+    <Box bgColor="gray.100" minH="100vh">
       <Header />
-      <chakra.main px={[5, 20]} py={5}>
+      <chakra.main px={[5, 20]} py={10}>
         <VStack align="left" spacing={10}>
           <UserInformations />
           <CompanyInformations />
+          <Accounts />
         </VStack>
       </chakra.main>
     </Box>
